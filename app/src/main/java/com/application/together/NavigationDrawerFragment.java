de52f6d7de9df3229f7107c621414f00f36412e1
Japorized <johnson5756@live.com>
2015-06-13 11:46:06 +0800

    Update #2: Strings

--- app/src/main/java/com/application/together/NavigationDrawerFragment.java
+++ app/src/main/java/com/application/together/NavigationDrawerFragment.java
@@ -102,9 +102,9 @@ public class NavigationDrawerFragment extends Fragment {
                 android.R.layout.simple_list_item_activated_1,
                 android.R.id.text1,
                 new String[]{
-                        getString(R.string.title_section1),
-                        getString(R.string.title_section2),
-                        getString(R.string.title_section3),
+                        getString(R.string.title_announcement),
+                        getString(R.string.title_neighbor),
+                        getString(R.string.title_profile),
                 }));
         mDrawerListView.setItemChecked(mCurrentSelectedPosition, true);
         return mDrawerListView;
